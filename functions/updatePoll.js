0ffd6de361a97765d1313e570a86bc934a34acd3
Mark Mooij <mark@ai-applied.nl>
2024-05-27 17:43:53 +0200

    updated to netlify post for quiz

---
+++ functions/updatePoll.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed'
+        };
+    }
+
+    try {
+        const pollDataPath = path.resolve(__dirname, '../data/polls.json');
+        const pollData = JSON.parse(fs.readFileSync(pollDataPath, 'utf8'));
+        
+        const newData = JSON.parse(event.body);
+		// Update poll data with newData
+        for (const key in newData) {
+            if (newData.hasOwnProperty(key)) {
+                pollData[key] = newData[key];
+            }
+        }
+
+        fs.writeFileSync(pollDataPath, JSON.stringify(pollData, null, 2));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Poll data updated successfully' }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to update poll data' }),
+        };
+    }
+};
