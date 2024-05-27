6851285ede811f8988a752cb286000ccabbbf6cb
Mark Mooij <mark@ai-applied.nl>
2024-05-27 08:52:04 +0200

    first commit

---
+++ quiz.js
@@ -0,0 +1,67 @@
+const quizLogic = [
+    {
+        element: "quiz",
+        question: "Guess the prompt!",
+		image: "1.png",
+        options: [
+            "Stock photo for a real estate advert",
+            "Photo of an American man in front of his house",
+            "Photo of a man in a neighbourhood"
+        ],
+        correctAnswer: 1
+    },
+    {
+        element: "quiz",
+        question: "Guess the prompt!",
+		image: "2.png",
+        options: [
+            "Photo of an African man in front of his house",
+            "Man standing near a hut",
+            "Photo from a film set"
+        ],
+        correctAnswer: 0
+    },
+    {
+        element: "poll",
+        question: "AI generated images are....",
+        options: ["Pretty weird", "Groundbreaking", "A work-in-progress"],
+    },
+    {
+        element: "quiz",
+        question: "Guess the prompt!",
+		image: "3.png",
+        options: [
+            "Photo of a strong woman",
+            "Photo of Miss America in the jungle",
+            "Photo of a natural woman"
+        ],
+        correctAnswer: 2
+    },
+    {
+        element: "poll",
+        question: "The biggest risk of AI-images is...",
+        options: [
+            "Photographers will be sidelined",
+            "Bias will continue",
+            "There are no risks"],
+    },
+    {
+        element: "quiz",
+        question: "Guess the prompt!",
+		image: "4.png",
+        options: [
+            "Photo of a future president",
+            "Photo of when kids rule the world",
+            "Photo of boy dressed in a suit"
+        ],
+        correctAnswer: 0
+    },
+    {
+        element: "poll",
+        question: "Do you think AI images are representative enough of minority groups?",
+        options: [
+            "Yes",
+            "No",
+            "Don't know"],
+    },
+]
